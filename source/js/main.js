f919b0d5ed760099f08fe5c4f7fb12b8b37582f4
Pavel Drow <paveldrow@yandex.ru>
2021-04-10 20:49:58 +0300

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,7 @@
+// const menu = document.querySelector('.main-nav');
+// const buttonMenu = document.querySelector('.button-menu');
+
+// buttonMenu.addEventListener('click', (evt) => {
+//   evt.preventDefault();
+//   menu.classList.toggle('display-none');
+// });
